b9dcab55ebfb2f401c76b12fe1bed5083d284f33
Ram Murat Sharma <rammurat.rms.sharma0@gmail.com>
2020-04-19 17:21:28 +0100

    custom filter added

---
+++ filter.js
@@ -0,0 +1,16 @@
+
+function map(list, cb) {
+    let _list = []
+
+    for(let i=0;i<list.length;i++){
+        if(cb(list[i],i,list)) {
+            _list.push(list[i])
+        }
+    }
+    return _list
+}
+
+const arrList = [{id:1,name:'John'}, {id:2,name:'Brian'}];
+console.log(map(arrList, (item)=> {
+    return item.id === 1;
+}))
